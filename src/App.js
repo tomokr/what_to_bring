815f2a8a4a7556b12f4ef13556d6ea59c325d565
tomokr <tomo@planna.in>
2021-08-03 21:31:48 -0400

    Checkbox appears

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleChange(e) {
+    // const newItem = {
+    //   itemName: inputValue,
+    //   quantity: 1,
+    //   isSelected: false,
+    // };
+    e.preventDefault();
+    const newItem = inputValue;
+
+    console.log(items);
+    // const newItems = [...items, newItem];
+
+    setItems((lists) => [...lists, newItem]);
+    // console.log(newItems);
+    console.log(newItem);
+    console.log(items);
+    setInputValue("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Shopping List</h1>
+      <div className="ShoppingListMain">
+        <div className="header">
+          <form onSubmit={handleChange}>
+            <input
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+            ></input>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      </div>
+
+      <div>
+        {items.length > 0 &&
+          items.map((item) => {
+            return (
+              <Fragment key={item.id}>
+                <input type="checkbox" id={item} key={item.id} />
+                <label key={"label" + item.id}>{item}</label>
+              </Fragment>
+            );
+          })}
+      </div>
+    </Fragment>
   );
 }
 

afb39310a4fd0097e9e46370ef27d98baa861584
tomokr <tomo@planna.in>
2021-12-04 21:16:28 -0500

    Enable to fetch data from Heroku rails server

--- src/components/atoms/ListMenu.tsx
+++ src/components/atoms/ListMenu.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 interface Props {
     listName: string;
+    listId: number;
     onClick?: any;
     className?: string;
 }
 
 const ListMenu : React.FC<Props> = ({
     listName,
+    listId,
     onClick,
     className,
     ...props
   }: Props) => {
-    const link = "/list/"+listName;
+    const link = "/list/"+listId+"/"+listName;
     return (
     <>
         <a href={link} onClick={onClick} className={className}>{listName}</a><br />
